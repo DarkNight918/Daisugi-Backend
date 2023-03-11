a10870ac7cf7ef979f73f3b1f8f8f8634b4e8476
Dark Night <darknight.dev0918@gmail.com>
2023-03-12 01:36:01 +0900

    Remove FE

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ app.use(async function (req, res, next) {
   next();
 });
 
-app.use(express.static('client'));
+// app.use(express.static('client'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/client/index.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/client/index.html');
+// });
 
 // Routes
 app.use('/api/coin', coinRouter);
