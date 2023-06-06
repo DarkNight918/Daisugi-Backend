e78a670db7e7fe1df8a317b3f285d989bad37ef5
Dark Night <darknight.dev0918@gmail.com>
2023-06-06 23:40:10 +0900

    MVP finished

--- controllers/funcController.js
+++ controllers/funcController.js
@@ -1,6 +1,9 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+const Investments = require("../models/investments")
+const ActiveInvestors = require("../models/active_investors")
+const Ventures = require('../models/ventures')
 
 exports.getLinkPreview = async (req, res) => {
 
@@ -27,3 +30,69 @@ exports.getLinkPreview = async (req, res) => {
     res.status(500).send('Error fetching link preview');
   }
 };
+
+exports.getInvestmentsData = async (req, res) => {
+  try {
+
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 25;
+
+    const totalCount = await Investments.countDocuments({});
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    const data = await Investments.find({})
+      .skip((page - 1) * pageSize)
+      .limit(pageSize)
+      .select('name date amount_raised round description lead_investor category source chains')
+    
+    return res.status(200).json({ totalPages, data });
+
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+exports.getActiveInvestors = async (req, res) => {
+  try {
+
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 25;
+
+    const totalCount = await ActiveInvestors.countDocuments({});
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    const data = await ActiveInvestors.find({})
+      .skip((page - 1) * pageSize)
+      .limit(pageSize)
+      .select('investor deals projects')
+    
+    return res.status(200).json({ totalPages, data });
+
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+exports.getVentures = async (req, res) => {
+  try {
+
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 25;
+
+    const totalCount = await Ventures.countDocuments({});
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    const data = await Ventures.find({})
+      .skip((page - 1) * pageSize)
+      .limit(pageSize)
+      .select('name description image')
+    
+    return res.status(200).json({ totalPages, data });
+
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
--- routes/func.js
+++ routes/func.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const funcController = require('../controllers/funcController');
 
 router.get('/link-preview', funcController.getLinkPreview)
+router.get('/investments', funcController.getInvestmentsData)
+router.get('/active_investors', funcController.getActiveInvestors)
+router.get('/ventures', funcController.getVentures)
 
 module.exports = router;
